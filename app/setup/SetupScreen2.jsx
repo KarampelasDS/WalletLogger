6fcacc714667e2bae51aa727cdbd5d63376c5df1
Dimitrios-Spiridon Karampelas <24dimitris@gmail.com>
2025-10-22 16:06:25 +0300

    Fixed bugs in the db creation

--- app/setup/SetupScreen2.jsx
+++ app/setup/SetupScreen2.jsx
@@ -1,17 +1,83 @@
 import { View, Text, StyleSheet } from "react-native";
 import { Store } from "../../stores/Store";
 import { useRouter } from "expo-router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Toast from "react-native-toast-message";
 import Button from "../../components/Button/Button";
 import SelectionScroller from "../../components/SelectionScroller/SelectionScroller";
 import ScrollerOption from "../../components/SelectionScroller/ScrollerOption";
+import * as SQLite from "expo-sqlite";
+import { openDatabaseAsync } from "expo-sqlite";
 
 export default function SetupScreen2() {
   const router = useRouter();
   const setShowNavbar = Store((state) => state.setShowNavbar);
   const [selectedCurrency, setSelectedCurrency] = useState(null);
+  const db = Store((state) => state.db);
   setShowNavbar(false);
+
+  useEffect(() => {
+    if (!db) return;
+
+    const createTables = async () => {
+      await db.execAsync("PRAGMA foreign_keys = ON;");
+
+      await db.execAsync(`
+      CREATE TABLE IF NOT EXISTS currencies (
+        currency_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        currency_name VARCHAR,
+        currency_symbol VARCHAR
+      )
+    `);
+
+      await db.execAsync(`
+      CREATE TABLE IF NOT EXISTS accounts (
+        account_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        account_name VARCHAR,
+        account_emoji VARCHAR,
+        account_balance DECIMAL
+      )
+    `);
+
+      await db.execAsync(`
+      CREATE TABLE IF NOT EXISTS categories (
+        category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        category_name VARCHAR,
+        category_emoji VARCHAR,
+        category_type VARCHAR,
+        category_order INTEGER
+      )
+    `);
+
+      await db.execAsync(`
+      CREATE TABLE IF NOT EXISTS transactions (
+        transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        transaction_type VARCHAR,
+        transaction_amount DECIMAL,
+        category_id INTEGER,
+        transaction_date DATETIME,
+        transaction_note VARCHAR,
+        account_id INTEGER,
+        account_from_id INTEGER,
+        account_to_id INTEGER,
+        currency_id INTEGER,
+        FOREIGN KEY (account_id) REFERENCES accounts(account_id),
+        FOREIGN KEY (account_from_id) REFERENCES accounts(account_id),
+        FOREIGN KEY (account_to_id) REFERENCES accounts(account_id),
+        FOREIGN KEY (category_id) REFERENCES categories(category_id),
+        FOREIGN KEY (currency_id) REFERENCES currencies(currency_id)
+      )
+    `);
+      const accounts = await db.getAllAsync("SELECT * FROM accounts");
+      console.log("Accounts:", accounts);
+      const categories = await db.getAllAsync("SELECT * FROM categories");
+      console.log("Categories:", categories);
+      const currencies = await db.getAllAsync("SELECT * FROM currencies");
+      console.log("Currencies:", currencies);
+    };
+
+    createTables();
+  }, []);
 
   const currencies = [
     // 🌍 Global Majors
